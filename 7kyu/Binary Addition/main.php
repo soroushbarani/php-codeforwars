6a95e2710a54dc9708e1d814303685f4952f1600
sorbarani <soroushbarani78@gmail.com>
2026-03-08 10:38:26 +0330

    feat:take numbers from user

--- 7kyu/Binary Addition/main.php
+++ 7kyu/Binary Addition/main.php
@@ -16,20 +16,17 @@
             while($num > 0)
             {
                 echo $num % 2 ."\n";
-            // $bin_digits[] = $num % 2;
                 array_splice($bin_digits, 0, 0, $num % 2);
                 $num = (int)($num / 2);
             }
 
-            // $bin_digits[] = $num; // for get the last digit.
-
-            // print_r($bin_digits);
             return implode("", $bin_digits);
         }
     }
 
-    $a = 51;
-    $b = 12;
+    echo "This programm adds two number then return the result in binary format.\n\n";
+    $a = readline("Enter first number:");
+    $b = readline("Enter second number:");
    
     echo add_binary($a, $b);
 ?>
